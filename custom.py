2926c162bd9567eb18d753a35d29e8345ba66333
Brussels Sprout <34946565+brussels-sprout@users.noreply.github.com>
2020-09-07 00:09:54 +0300

    Update custom.py
    Rename bank "bets" to "active_bets" and "wallet" to "balance"

--- custom.py
+++ custom.py
@@ -1,18 +1,13 @@
 import json
 
 
-async def open_account(user):
-    bank_data = await get_bank_data()
-
-    if str(user.id) in bank_data:
-        return
-
-    bank_data[str(user.id)] = {}
-    bank_data[str(user.id)]["wallet"] = 100
-    bank_data[str(user.id)]["bets"] = 0
-
-    with open("bank.json", "w") as file:
-        json.dump(bank_data, file)
+# bank format: {
+#   "[user_id]": {
+#       "balance": [number of bedcoins],
+#       "active_bets": [number of active bets]
+#   },
+#   ...
+# }
 
 
 async def get_bank_data():
@@ -20,3 +15,18 @@ async def get_bank_data():
         bank_data = json.load(file)
 
     return bank_data
+
+
+# opens an account if the user does not have one already
+async def open_account(user):
+    bank_data = await get_bank_data()
+
+    if str(user.id) in bank_data:
+        return
+
+    bank_data[str(user.id)] = {}
+    bank_data[str(user.id)]["balance"] = 100
+    bank_data[str(user.id)]["active_bets"] = 0
+
+    with open("bank.json", "w") as file:
+        json.dump(bank_data, file)
